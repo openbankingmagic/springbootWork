f1b336ba81c8c7ef473fea1c6193aee65ae4b70f
outtalaw@naver.com <JYW>
2020-02-06 12:47:20 +0900

    스프링부트 블로그 화면 세팅 완료

--- blog/src/main/java/com/cos/blog/repository/CommentRepository.java
+++ blog/src/main/java/com/cos/blog/repository/CommentRepository.java
@@ -3,6 +3,6 @@ package com.cos.blog.repository;
 import java.util.List;
 
 // MapperScan에 의해서 메모리에 로드됨. <-컴포넌트 스캔될때 컨피규레이션이 되니까 데이터엑세스콘픽파일보면
-public interface UserRepository {
+public interface CommentRepository {
 
 }
