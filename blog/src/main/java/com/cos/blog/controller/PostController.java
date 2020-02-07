118ee96642f92ce64d706991ecf98dad2d2fd8cc
outtalaw@naver.com <JYW>
2020-02-07 14:49:49 +0900

    스프링 인터셉터 인증 체크 완료 - 버그 수정

--- blog/src/main/java/com/cos/blog/controller/PostController.java
+++ blog/src/main/java/com/cos/blog/controller/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cos.blog.model.user.User;
 
@@ -33,15 +34,15 @@ public class PostController {
 	}
 
 	// 인증 체크, 동일인 체크
-	@GetMapping("/post/update/{id}")
-	public String update(@PathVariable int id) {
+	@GetMapping("/post/update/{postId}")
+	public String update(@PathVariable int postId, @RequestParam int userId) {
 
 		User principal = (User) session.getAttribute("principal");
 
-		if (principal.getId() == id) {
-			return "/post/update";
-		} else {
+		if (principal.getId() == userId) {
 			return "/user/login";
 		}
+		// postId로 select 해서 post 가져오기 필요 -Model에 담기 필요
+		return "/post/update";
 	}
 }
